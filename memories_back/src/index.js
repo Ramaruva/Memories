6a60e7497812025a9ef43f40a5d207b3f21e5184
Ramaruva <ramrj1428@gmail.com>
2021-08-24 17:54:47 +0530

    index

--- memories_back/src/index.js
+++ memories_back/src/index.js
@@ -1,1 +1,13 @@
-console.log("hi")
+const express = require("express")
+const bodyParser=require("body-parser")
+const mongoose = require("mongoose")
+const cors= require("cors")
+
+const app=express()
+
+app.use(bodyParser.json({limit:'30mb',extended:true}))
+app.use(bodyParser.urlencoded({limit:'30mb',extended:true}))
+app.use(cors())
+
+
+//
