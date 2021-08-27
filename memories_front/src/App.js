c8c1f912c4b2cf34402da03891871896ee8cc02b
Ramaruva <ramrj1428@gmail.com>
2021-08-27 16:21:32 +0530

    added basic jsx structure for front

--- memories_front/src/App.js
+++ memories_front/src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Container from "@material-ui/core/Container";
+import { AppBar, Grid, Grow, Typography } from "@material-ui/core";
+import { Posts } from "./components/posts/Posts";
+import { Form } from "./components/form/Form";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container>
+        <AppBar position="static" color="inherit">
+          <Typography variant="h2" align="center">
+            Memories
+          </Typography>
+          <img
+            src="https://raw.githubusercontent.com/adrianhajdin/project_mern_memories/master/client/src/images/memories.png?token=AF56X74XONEUGZ4FD2FUIA27UURPI"
+            alt="memories"
+            height="60"
+          />
+        </AppBar>
+        <Grow in>
+          <Container>
+            <Grid
+              container
+              justify="space-between"
+              alignItems="stretch"
+              spacing={3}
+            >
+              <Grid xs={12} sm={7}>
+                <Posts />
+              </Grid>
+              <Grid xs={12} sm={4}>
+                <Form />{" "}
+              </Grid>
+            </Grid>
+          </Container>
+        </Grow>
+      </Container>
+    </>
   );
 }
 
---
+++ memories_front/src/components/posts/Posts.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Post } from './post/Post'
+
+export const Posts = () => {
+    return (
+        <div>
+            posts
+            <Post />
+            <Post />
+        </div>
+    )
+}
