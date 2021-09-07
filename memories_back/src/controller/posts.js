837f799a3730a3193b52cf6ed31c99045a586a42
Ramaruva <ramrj1428@gmail.com>
2021-09-07 17:22:55 +0530

    added

--- memories_back/src/controller/posts.js
+++ memories_back/src/controller/posts.js
@@ -18,7 +18,19 @@ const createPosts =async(req,res)=>
         return res.status(409).json({message:error.message})
     }
 }
+const updatPosts =async(req,res)=>
+{
+    try {
+          const {id:_id}=req.params
+          const post=req.body
+          const update= await postMessage.findByIdAndUpdate(_id,post,{new:true});
+          return res.status(200).json(update)
+    } catch (error) {
+        return res.status(409).json({message:error.message})
+    }
+}
 module.exports={
     getPosts,
-    createPosts
+    createPosts,
+    updatPosts
 }
--- memories_back/src/routes/post.js
+++ memories_back/src/routes/post.js
@@ -1,8 +1,9 @@
 const express = require("express")
-const {getPosts,createPosts} =require("../controller/posts")
+const {getPosts,createPosts, updatPosts} =require("../controller/posts")
 const router = express.Router()
 
 router.get("/",getPosts)
 router.post("/",createPosts)
+router.patch("/:id",updatPosts)
 
 module.exports=router
