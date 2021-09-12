f0f9b2373fa0b2ef0ab331c19f1a70861db9ef88
Ramaruva <ramrj1428@gmail.com>
2021-09-12 16:08:11 +0530

    changes

--- memories_front/src/redux/posts/postReducer.js
+++ memories_front/src/redux/posts/postReducer.js
@@ -1,10 +1,18 @@
-import { POST_FAILURE, POST_LOADING, POST_SUCCESS, SINGLE_POST_FAILURE, SINGLE_POST_LOADING, SINGLE_POST_SUCCESS, SINGLE_POST_UPDATE } from "./postactionTypes";
+import {
+  POST_FAILURE,
+  POST_LOADING,
+  POST_SUCCESS,
+  SINGLE_POST_FAILURE,
+  SINGLE_POST_LOADING,
+  SINGLE_POST_SUCCESS,
+  SINGLE_POST_UPDATE,
+} from "./postactionTypes";
 
 const initstate = {
   ploading: false,
   pfailure: false,
-  sloading:false,
-  sfailure:false,
+  sloading: false,
+  sfailure: false,
   psuccess: [],
 };
 
@@ -28,29 +36,31 @@ export const postReducer = (state = initstate, { type, payload }) => {
         pfailure: true,
       };
     case SINGLE_POST_LOADING:
-      return{
-        ...state,
-        sloading:true
-      }
-    case SINGLE_POST_FAILURE:
-      return{
-        ...state,
-        sloading:false,
-        sfailure:true
-      }  
-    case SINGLE_POST_SUCCESS:
-      return{
-        ...state,
-        sloading:false,
-        psuccess:[...state.psuccess,payload]
-      }  
-    case SINGLE_POST_UPDATE:
-      const array=state.psuccess?.map((p)=>p._id===payload._id?payload:p)
       return {
         ...state,
-        sloading:false,
-        psuccess:array
-      }
+        sloading: true,
+      };
+    case SINGLE_POST_FAILURE:
+      return {
+        ...state,
+        sloading: false,
+        sfailure: true,
+      };
+    case SINGLE_POST_SUCCESS:
+      return {
+        ...state,
+        sloading: false,
+        psuccess: [...state.psuccess, payload],
+      };
+    case SINGLE_POST_UPDATE:
+      const array = state.psuccess?.map((p) =>
+        p._id === payload._id ? payload : p
+      );
+      return {
+        ...state,
+        sloading: false,
+        psuccess: array,
+      };
     default:
       return state;
   }
